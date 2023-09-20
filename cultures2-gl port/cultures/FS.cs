77358d7949f6335664445bcfdd9cb614212205ad
remik0495 <k.szwed@etqa.pl>
2023-09-20 17:28:49 +0200

    port

---
+++ cultures2-gl port/cultures/FS.cs
@@ -0,0 +1,79 @@
+﻿using cultures2_gl_port.cultures.models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.WebRequestMethods;
+
+namespace cultures2_gl_port.cultures
+{
+    public class FS
+    {
+        FSHeader header;
+        List<FSDirInfo> dirs;
+        List<FSFileInfo> files;
+        FileStream fs;
+        BinaryReader buffer;
+        public FS(string file)
+        {
+            fs = new FileStream(file,FileMode.Open, FileAccess.Read);
+            fs.Seek(0, SeekOrigin.Begin);
+            BinaryReader buffer = new BinaryReader(fs);
+            
+            header = getHeader(buffer);
+            getDirs(header.num_dirs, buffer);
+            getFiles(header.num_files, buffer);
+        }
+
+        private FSHeader getHeader(BinaryReader fs)
+        {
+            return new FSHeader { version = fs.ReadUInt32(), num_dirs = fs.ReadUInt32(), num_files = fs.ReadUInt32() };
+        }
+
+        private void getDirs(UInt32 n, BinaryReader fs) 
+        {
+            dirs = new List<FSDirInfo>();
+            for (int i = 0; i < n; i++)
+            {
+                dirs.Add(new FSDirInfo { path = GetString(fs), depth = fs.ReadUInt32() });
+            }
+        }
+
+        public FSFileInfo GetFileInfo(string path)
+        {
+            FSFileInfo info= files.FirstOrDefault(f => f.path == path);
+            if (info == null) throw new Exception("File not found: " + path);
+
+            return info;
+        }
+
+        public byte[] Open(string path)
+        {
+            FSFileInfo fi = GetFileInfo(path);
+            fs.Seek(fi.offset, SeekOrigin.Begin);
+
+            return buffer.ReadBytes(Convert.ToInt32(fi.offset));
+        }
+
+        private string GetString(BinaryReader br)
+        {
+            UInt32 lenght = br.ReadUInt32();
+            return System.Text.Encoding.ASCII.GetString( br.ReadBytes(Convert.ToInt32(lenght)));
+        }
+
+        private void getFiles(UInt32 n, BinaryReader fs)
+        {
+            files = new List<FSFileInfo>();
+            for (int i = 0; i < n; i++)
+            {
+                files.Add(new FSFileInfo { path = GetString(fs), offset = fs.ReadUInt32(),length= fs.ReadUInt32() });
+            }
+        }
+
+
+
+
+    }
+}
---
+++ cultures2-gl port/cultures/models/FSmodels.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cultures2_gl_port.cultures.models
+{
+    public class FSHeader
+    {
+        public UInt32 version { get; set; }
+        public UInt32 num_dirs { get; set; }
+        public UInt32 num_files { get; set; }
+    }
+
+    public class FSDirInfo
+    {
+        public string path { get; set; }
+        public UInt32 depth { get; set; }
+    }
+
+    public class FSFileInfo
+    {
+        public string name { get; set; }
+        public string path { get; set; }
+        public UInt32 offset { get; set; }
+        public UInt32 length { get; set; }
+    }
+}
